ced96906ab93c737039c7a265680fdef79a4cd22
zerochae <zerochae@kakao.com>
2022-02-23 03:03:37 +0900

    Modify: semantic tag css

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-document.createElement('header');
-document.createElement('section');
-document.createElement('nav');
-document.createElement('footer');
-
 // favicon set
 (function() {
   var link = document.createElement('link');
