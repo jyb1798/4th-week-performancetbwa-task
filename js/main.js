074587bc67fbca32e2bba7561e9c4735935dc1ee
zerochae <zerochae@kakao.com>
2022-02-23 02:46:25 +0900

    Modify: semantic tag css

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// favicon set
+(function() {
+  var link = document.createElement('link');
+  link.type = 'image/x-icon';
+  link.rel = 'shortcut icon';
+  link.href = '/images/favicon.ico';
+  document.getElementsByTagName('head')[0].appendChild(link);
+}());
+//
