c032e9c5f6434684c139a52ea5751ef909bb1e19
zerochae <zerochae@kakao.com>
2022-02-22 20:59:45 +0900

    Fix: Footer NavBar Position 크로스 브라우징 대응 수정

--- js/footer.js
+++ js/footer.js
@@ -8,9 +8,9 @@ if (window.addEventListener) {
       : mobileNav.classList.remove("isFooter");
   });
 } else {
-  window.attachEvent("scroll", function () {
+  window.onscroll = function () {
     footer.getBoundingClientRect().top + 500 <= this.window.scrollY
       ? mobileNav.classList.add("isFooter")
       : mobileNav.classList.remove("isFooter");
-  });
+  };
 }
